6c71657b78b116f7b6f2f478973b5c573e39f3e5
Ovie <ovie04@gmail.com>
2026-01-31 09:11:54 -0500

    add redirect, new mongo string

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const app = express();
 const PORT = 3000;
@@ -13,8 +14,34 @@ const JWT_SECRET = 'your-secret-key-change-in-production';
 app.use(cors());
 app.use(express.json());
 
+const uri = "mongodb+srv://anniovie:Theklaehn@klaehnomene.rpmxuwh.mongodb.net/?appName=KlaehnOmene";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// const client = new MongoClient(uri, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   }
+// });
+
+// async function run() {
+//   try {
+//     // Connect the client to the server	(optional starting in v4.7)
+//     await client.connect();
+//     // Send a ping to confirm a successful connection
+//     await client.db("admin").command({ ping: 1 });
+//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     await client.close();
+//   }
+// }
+// run().catch(console.dir);
+
+
 // MongoDB Connection
-mongoose.connect('mongodb://localhost:27017/', {
+mongoose.connect(uri, {
   // useNewUrlParser: true,
   // useUnifiedTopology: true
 }).then(() => console.log('MongoDB connected'))
